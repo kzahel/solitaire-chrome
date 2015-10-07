4e842bae52cf763e15457f8021bf99e7cb4e616e
Kyle Graehl <kgraehl@gmail.com>
2015-10-06 20:13:31 -0700

    first commit

--- package.sh
+++ package.sh
@@ -1,2 +1,2 @@
 rm package.zip
-zip package.zip background.js icons/*.png index.html index.js manifest.json OfflineBrowserGames/war/*.html OfflineBrowserGames/war/*.css OfflineBrowserGames/war/*.js
+zip package.zip background.js icons/*.png index.html index.js manifest.json OfflineBrowserGames/war/*.html OfflineBrowserGames/war/*.css OfflineBrowserGames/war/*.js OfflineBrowserGames/war/*.png
